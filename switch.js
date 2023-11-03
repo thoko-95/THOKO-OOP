9eaa0d424597649b6f98e80c8fc6a9b71c5e7f0e
thoko-95 <thokomgidi1@gmail.com>
2023-11-03 14:31:49 +0200

    added

---
+++ switch.js
@@ -0,0 +1,38 @@
+// let grade = 'A';
+// let result;
+
+// switch (grade) 
+// {
+//     case 'A':
+//     result = "A (Excellent)";
+//         break;
+//     case 'B':
+//     result = "B(Average)";
+//     break;
+//     case 'C':
+//     result = "C (Below than average)";
+//     break;
+//     default:
+//     result = "No Grade";
+     
+// }
+    
+//     console.log(result);
+
+let passMark = 90;
+
+switch (passMark) 
+{
+    case 30:
+     console.log('failed');
+        break;
+    case 60:
+     console.log('passed');
+    break;
+    case 90:
+    default:
+     console.log('marvellous');;
+     
+}
+    
+    //console.log(result);
