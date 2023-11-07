5b17f9f32bff7b10c3efb2baa7291a8a8868625c
thoko-95 <thokomgidi1@gmail.com>
2023-11-07 11:52:30 +0200

    added

--- getters_and_setters.js
+++ getters_and_setters.js
@@ -3,16 +3,25 @@ const student = {
     //data property
     firstName : 'Thokozile',
     surname : 'Mgidi',
+    Contants : '0827835761',
+    age : '56',
     //Accessor property(getter)
     get getName()
     {
-        return this.firstName;
+        return this.firstName;//dot notation with this for singular object
         return this.surname;
+        return this.Contants;
+        //return [this.firstName ,this.surname ];//square brackets to return many things or information
     },
 
     set changeName(newName)
     {
         this.firstName=newName
+    },
+
+    get getDetails(){
+
+            return [this.firstName, this.surname , this.age , this.Contants];
     }
  
 
@@ -22,6 +31,9 @@ const student = {
 console.log(student.firstName);
 console.log(student.getName)
 console.log(student.surname)
+//call everything
+
+console.log(student.getDetails);
 student.changeName = "Thoko";
 
 
